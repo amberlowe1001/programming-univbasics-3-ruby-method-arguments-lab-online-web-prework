ae15fc4a11f592f39f54c823d4f8379960569928
lite-post-0822 <amberlowe40@gmail.com>
2019-07-11 05:24:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
-def #introduction "Hi, my name is your+{name}.""
-puts "enter your name"
-gets= 
+def introduction =["Hi, my name is your +{name}]
+name = amber
+puts "Hi my name is" + name
+
+  end
+end
